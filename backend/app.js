f291cda5f5a227a1199af8bc3937bfdac1635868
2003devansh <faujdardevansh@gmail.com>
2025-03-13 22:17:41 +0530

    Implement post routes and controller with Mongoose schema for posts

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const postRoutes = require("./routes/post") ;
 
 const app = express();
 
@@ -8,8 +9,8 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use("/posts",postRoutes);
+// this mean that any route that start with /posts will be redirected to postRoutes
+
 
 module.exports = app;
---
+++ backend/controllers/postController.js
@@ -0,0 +1,5 @@
+const getPosts = async (req, res) => {
+    res.send('Hello World');
+}
+
+module.exports = getPosts;
