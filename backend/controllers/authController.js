58c3b6a835ae6d09aaffe0b2e92ffb644c7ca7e1
2003devansh <faujdardevansh@gmail.com>
2025-03-17 22:58:49 +0530

    feat: implement user signup with email existence check and password hashing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,8 +3,21 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user') ;
 
 const signup =  async (req,res)=>{
-
     const {name,email,password} = req.body  ; 
-    
+
+    try {
+        const existingUser = await User.findOne({email}) ;
+        if(existingUser){
+            return res.status(400).json({message:"User already exists"}) ;
+        }
+
+        const hashedPassword = await bcrypt.hash(password,12) ;
+        const result  = await User.create({email,password:hashedPassword,name}) ;
+        const token  = jwt.sign({email:result.email , id: result._id},process.env.JWT_SECRET,{expiresIn:"1h"}) ;
+
+    } catch (error) {
+        
+    }
+
 
 }
